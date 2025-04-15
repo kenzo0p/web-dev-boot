29b7445d62f92365eac7f8ef76ed002877a75085
Om Bhor <ombhor617@gmail.com>
2025-04-15 12:08:12 +0530

    Week-7 backend user/admin signup/login

--- week-7/server/routes/admin.route.js
+++ week-7/server/routes/admin.route.js
@@ -1,12 +1,52 @@
 const { Router } = require("express");
+const { adminModel } = require("../models/admin.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 const adminRouter = Router();
 
-adminRouter.post("/admin/signup", (req, res) => {
-  // logic to sign up admin
+adminRouter.post("/signup", async (req, res) => {
+  try {
+    const { email, firstName, lastName, password } = req.body;
+    if (!email || !firstName || !lastName || !password) {
+      return res.status(400).json("All fields are required");
+    }
+    const existedUser = await adminModel.findOne({ email });
+    if (existedUser) {
+      return res.status(400).json("User already exist with this email");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await adminModel.create({
+      email,
+      firstName,
+      lastName,
+      password: hashPassword,
+    });
+    if (!newUser) {
+      return res.status(400).json("Something went wrong");
+    }
+    return res
+      .status(201)
+      .json({ newUser, Message: "User created successfulyy" });
+  } catch (error) {
+    console.log(error, "Error while signup user");
+    return res.status(500).json("Internal server error");
+  }
 });
 
-adminRouter.post("/admin/login", (req, res) => {
-  // logic to log in admin
+adminRouter.post("/login", async (req, res) => {
+  // logic to log in user
+  try {
+    const { email, password } = req.body;
+    const admin = await adminModel.findOne({ email });
+    const isPassWord = await bcrypt.compare(password, admin.password);
+    if (!isPassWord) {
+      return res.status(400).json("Password is wrong");
+    }
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET_ADMIN);
+    return res.json({ token: token });
+  } catch (error) {
+    console.log(error, "Login error");
+  }
 });
 
 adminRouter.post("/admin/courses", (req, res) => {
@@ -21,4 +61,4 @@ adminRouter.get("/admin/courses", (req, res) => {
   // logic to get all courses
 });
 
-module.exports =  adminRouter;
+module.exports = adminRouter;
--- week-7/server/routes/user.route.js
+++ week-7/server/routes/user.route.js
@@ -1,12 +1,53 @@
 const { Router } = require("express");
+const { userModel } = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userRouter = Router();
 
-userRouter.post("/users/signup", (req, res) => {
+userRouter.post("/signup", async (req, res) => {
   // logic to sign up user
+  try {
+    const { email, firstName, lastName, password } = req.body;
+    if (!email || !firstName || !lastName || !password) {
+      return res.status(400).json("All fields are required");
+    }
+    const existedUser = await userModel.findOne({ email });
+    if (existedUser) {
+      return res.status(400).json("User already exist with this email");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await userModel.create({
+      email,
+      firstName,
+      lastName,
+      password: hashPassword,
+    });
+    if (!newUser) {
+      return res.status(400).json("Something went wrong");
+    }
+    return res
+      .status(201)
+      .json({ newUser, Message: "User created successfulyy" });
+  } catch (error) {
+    console.log(error, "Error while signup user");
+    return res.status(500).json("Internal server error");
+  }
 });
 
-userRouter.post("/users/login", (req, res) => {
+userRouter.post("/login", async (req, res) => {
   // logic to log in user
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    const isPassWord = await bcrypt.compare(password, user.password);
+    if (!isPassWord) {
+      return res.status(400).json("Password is wrong");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_USER);
+    return res.json({ token: token });
+  } catch (error) {
+    console.log(error, "Login error");
+  }
 });
 
 userRouter.get("/users/courses", (req, res) => {
